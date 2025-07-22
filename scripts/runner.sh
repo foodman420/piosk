f7665f72dd3d81295727da0cc79be91a96fed519
Jack Goodman <116262865+foodman420@users.noreply.github.com>
2025-07-22 11:04:06 +0100

    Update runner.sh

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-chromium-browser \
-  $(jq -r '.urls | map(.url) | join(" ")' /opt/piosk/config.json) \
-  --disable-component-update \
-  --disable-infobars \
-  --disable-session-crashed-bubble \
-  --disable-features=AllowCrossOriginAuthPrompt,SameSiteByDefaultCookies \
-  --auth-server-whitelist="*csmsvr02/" \
-  --auth-negotiate-delegate-whitelist="*csmsvr02/" \
-  --enable-auth-negotiate-port \
-  --no-first-run \
-  --noerrdialogs \
-  --kiosk
+
+# Read URLs from config
+URLS=$(jq -r '.urls | map(.url) | join(" ")' /opt/piosk/config.json)
+
+# Launch Firefox in kiosk mode with URLs
+firefox \
+  --kiosk $URLS \
+  --no-remote \
+  --setDefaultBrowser \
+  --new-window
